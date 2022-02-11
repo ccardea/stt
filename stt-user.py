701e62d860b0e10a76a26824da38e3936fdcfdfb
Christopher Cardea <ccardea@gmail.com>
2022-02-11 11:50:51 -0500

    Design user interface

---
+++ stt-user.py
@@ -0,0 +1,73 @@
+"""
+Simple Time Tracker
+Author C. Cardea
+Created 2022-02-11
+"""
+def chooseProject(projects):
+    """
+    Choose a project from a list
+    Parameters:
+        projects: list
+    Returns: integer
+        index of projects list
+    """
+    return
+
+def chooseActivity(activities):
+    """
+    Choose an activity from a list
+    Parameters:
+        activities: list
+    Returns: integer
+        index of choices list
+    """
+    return
+
+def chooseStart(choices):
+    """
+    Choose to start or exit
+    Parameters:
+        choices: list
+    Returns: integer index of choices list
+    """
+    return
+
+def chooseStop(choices):
+    """
+    Allows user to stop or pause recording the current activity
+    Returns: integer indexof choices list
+    """
+    return
+
+def getComment():
+    """
+    Get comment from user input
+    Returns: string
+    """
+
+def chooseResume(choices):
+    """
+    Allow user to resume current activity or stop
+    Parameters:
+        choices: list
+    Returns: integer index of choices list
+    """
+    return
+
+def chooseNext(choices):
+    """
+    Choose next action from new activity, new project, quit program
+    Parameters:
+        choices: list
+    Returns: integer index of choices list
+    """
+    return
+
+def getInput(choices, prompt):
+    """
+    Get Input from user
+    Parameters:
+        choices: list
+        prompt: string input prompt
+    Returns: integer index of choices list
+    """

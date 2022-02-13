52ea1137fb7de69f70abfc6bf335750ae6c1cc93
Christopher Cardea <ccardea@gmail.com>
2022-02-12 19:49:05 -0500

    Changed user to a class

--- stt_user.py
+++ stt_user.py
@@ -3,114 +3,114 @@ Simple Time Tracker
 Author C. Cardea
 Created 2022-02-11
 """
-from socketserver import ThreadingUnixDatagramServer
 
+class TerminalUser():
 
-def chooseProject(projects):
-    """
-    Choose a project from a list
-    Parameters:
-        projects: list
-    Returns: integer
-        index of projects list
-    """
-    print("Please choose a project:")
-    return getInput(projects)
+    def chooseProject(self, projects):
+        """
+        Choose a project from a list
+        Parameters:
+            projects: list
+        Returns: integer
+            index of projects list
+        """
+        print("Please choose a project:")
+        return self.getInput(projects)
 
-def chooseActivity(activities):
-    """
-    Choose an activity from a list
-    Parameters:
-        activities: list
-    Returns: integer
-        index of choices list
-    """
-    print("Please choose an activity:")
-    return getInput(activities)
+    def chooseActivity(self,activities):
+        """
+        Choose an activity from a list
+        Parameters:
+            activities: list
+        Returns: integer
+            index of choices list
+        """
+        print("Please choose an activity:")
+        return self.getInput(activities)
 
-def chooseStart(choices):
-    """
-    Choose to start or exit
-    Parameters:
-        choices: list
-    Returns: integer index of choices list
-    """
-    print("Please choose start to begin tracking.")
-    return getInput(choices)
-
-def chooseStop(choices):
-    """
-    Allows user to stop or pause recording the current activity
-    Returns: integer index of choices list
-    """
-    print("Please choose stop to end or pause to return later")
-    return getInput(choices)
-
-def getComment():
-    """
-    Get comment from user input
-    Returns: string
-    """
-    print("Please enter a comment (50 characters or less).")
-    while True:
-        comment = input(">>")
-        if not validate(comment):
-            print("Printable alphanumeric characters only please.")
-            continue
-        else:
-            break
-    if len(comment) <= 50:
-        return comment
-    else:
-        return comment[0:50]
-
-def validate(comment):
-    words = comment.split()
-    for word in words:
-        if not word.isalnum() or not word.isprintable():
-            return False
-    return True 
-
-
-def chooseResume(choices):
-    """
-    Allow user to resume previous activity or stop
-    Parameters:
-        choices: list
-    Returns: integer index of choices list
-    """
-    print("Resume previous activity or stop")
-    return getInput(choices)
-
-def chooseNext(choices):
-    """ 
-    Choose next action (new activity, new project, quit program).
+    def chooseStart(self,choices):
+        """
+        Choose to start or exit
         Parameters:
             choices: list
         Returns: integer index of choices list
-    """
-    print("Where to next?")
-    return getInput(choices)
+        """
+        print("Please choose start to begin tracking.")
+        return self.getInput(choices)
 
-def getInput(choices):
-    """
-    Get Input from user
-    Parameters:
-        choices: list
-        prompt: string input prompt
-    Returns: integer index of choices list
-    """
-    prompt = "Please enter the number that identifies your choice: "
-    for a in enumerate(choices):
-            print(a[0], ":", a[1])
-    while True:
-        choice = input(prompt)
-        if not choice.isdigit():
-            print("What was that?")
-            continue
-        elif int(choice) >= len(choices) or int(choice) < 0:
-            print("What was that?")
-            continue
+    def chooseStop(self, choices):
+        """
+        Allows user to stop or pause recording the current activity
+        Returns: integer index of choices list
+        """
+        print("Please choose stop to end or pause to return later")
+        return self.getInput(choices)
+
+    def getComment(self):
+        """
+        Get comment from user input
+        Returns: string
+        """
+        print("Please enter a comment (50 characters or less).")
+        while True:
+            comment = input(">>")
+            if not self.validate(comment):
+                print("Printable alphanumeric characters only please.")
+                continue
+            else:
+                break
+        if len(comment) <= 50:
+            return comment
         else:
-            break
-    return int(choice)
+            return comment[0:50]
+
+    def validate(self, comment):
+        words = comment.split()
+        for word in words:
+            if not word.isalnum() or not word.isprintable():
+                return False
+        return True 
+
+
+    def chooseResume(self, choices):
+        """
+        Allow user to resume previous activity or stop
+        Parameters:
+            choices: list
+        Returns: integer index of choices list
+        """
+        print("Resume previous activity or stop")
+        return self.getInput(choices)
+
+    def chooseNext(self, choices):
+        """ 
+        Choose next action (new activity, new project, quit program).
+            Parameters:
+                choices: list
+            Returns: integer index of choices list
+        """
+        print("Where to next?")
+        return self.getInput(choices)
+
+    def getInput(self, choices):
+        """
+        Get Input from user
+        Parameters:
+            choices: list
+            prompt: string input prompt
+        Returns: integer index of choices list
+        """
+        prompt = "Please enter the number that identifies your choice: "
+        for a in enumerate(choices):
+                print(a[0], ":", a[1])
+        while True:
+            choice = input(prompt)
+            if not choice.isdigit():
+                print("What was that?")
+                continue
+            elif int(choice) >= len(choices) or int(choice) < 0:
+                print("What was that?")
+                continue
+            else:
+                break
+        return int(choice)
--- tests/user_tests.py
+++ tests/user_tests.py
@@ -5,10 +5,15 @@ Created 02-10-2022
 Tests for input functions,  unit tests might work here
 """
 
-import stt_user as user
+import stt_user
 import unittest
 
 class UserTestCase(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.user = stt_user.TerminalUser()
+        return None
 
     def setUp(self):
         self.choices = ["option1", "option2","option3"]
@@ -16,52 +21,57 @@ class UserTestCase(unittest.TestCase):
 
     # @unittest.skip("Skipped testGetInput")
     def testGetInput(self):   
-        choice = user.getInput(self.choices)
+        choice = self.user.getInput(self.choices)
         self.assertIsInstance(choice,int)
         self.assertTrue(choice < len(self.choices))
         return None
 
     def testChooseProject(self):
-        choice = user.chooseProject(self.choices)
+        choice = self.user.chooseProject(self.choices)
         self.assertIsInstance(choice,int)
         self.assertTrue(choice < len(self.choices))
         return None
 
     def testChooseActivity(self):
-        choice = user.chooseActivity(self.choices)
+        choice = self.user.chooseActivity(self.choices)
         self.assertIsInstance(choice,int)
         self.assertTrue(choice < len(self.choices))
         return None
     
     def testChooseStart(self):
-        choice = user.chooseStart(self.choices)
+        choice = self.user.chooseStart(self.choices)
         self.assertIsInstance(choice,int)
         self.assertTrue(choice < len(self.choices))
         return None
     
     def testChooseStop(self):
-        choice = user.chooseStop(self.choices)
+        choice = self.user.chooseStop(self.choices)
         self.assertIsInstance(choice,int)
         self.assertTrue(choice < len(self.choices))
         return None
     
     def testChooseResume(self):
-        choice = user.chooseResume(self.choices)
+        choice = self.user.chooseResume(self.choices)
         self.assertIsInstance(choice,int)
         self.assertTrue(choice < len(self.choices))
         return None
     
     def testChooseNext(self):
-        choice = user.chooseNext(self.choices)
+        choice = self.user.chooseNext(self.choices)
         self.assertIsInstance(choice,int)
         self.assertTrue(choice < len(self.choices))
         return None
     
 class UserTestCase2(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        cls.user = stt_user.TerminalUser()
+        return None
+
     # @unittest.skip("Skipped testGetComent")
     def testGetComment(self):
-        comment = user.getComment()
+        comment = self.user.getComment()
         self.assertIsInstance(comment, str)
         self.assertTrue(comment.isprintable())
         self.assertTrue(len(comment) <= 50)
